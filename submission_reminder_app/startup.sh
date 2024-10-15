0592bf087a35216c14741d03aaba08f5ec36b66c
GAMA <a.mugisha1@alustudent.com>
2024-10-15 11:07:22 +0200

    Delete submission_reminder_app directory

--- submission_reminder_app/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Source the config file
-source submission_reminder_app/config/config.env
-
-# Start the reminder application
-bash submission_reminder_app/scripts/reminder.sh
-
-echo "Reminder app started."
